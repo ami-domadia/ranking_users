8eff74406457f5e8ecbc5e19803bff195ba8fb83
Ami Domadia <ami@Amis-MacBook-Pro.local>
2019-04-01 19:55:22 -0400

    heroku

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Sequelize = require('Sequelize');
+const Sequelize = require('sequelize');
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/ranking_users';
 
 const orm = new Sequelize(DATABASE_URL, {logging: false});
